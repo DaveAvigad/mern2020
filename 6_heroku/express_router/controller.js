3ad467723cadadc064e15d6873f269f923356405
David Avigad <david@lusha.co>
2019-12-01 01:10:44 +0200

    new files

--- 6_heroku/express_router/controller.js
+++ 6_heroku/express_router/controller.js
@@ -1,56 +1,56 @@
 const data = require('./data.json');
 
 exports.resturantController = {
-  get(req, res) {
-    if (req.params && req.params.id) {
-      const { id } = req.params;
+    get(req, res) {
+        if (req.params && req.params.id) {
+            const { id } = req.params;
 
-      console.log('single data requested');
-      res.json(data[id - 1]);
-    } else {
-      console.log('all data requested');
-      res.json(data);
-    }
-  },
-  post(req, res) {
-    console.log('new entity saved!');
+            console.log('single data requested');
+            res.json(data[id - 1]);
+        } else {
+            console.log('all data requested');
+            res.json(data);
+        }
+    },
+    post(req, res) {
+        console.log('new entity saved!');
 
-    const { body } = req;
+        const { body } = req;
 
-    data.push(body);
-    res.send('new entity saved!');
-  },
-  put(req, res) {
-    const { id } = req.params;
-    const { body } = req;
+        data.push(body);
+        res.send('new entity saved!');
+    },
+    put(req, res) {
+        const { id } = req.params;
+        const { body } = req;
 
-    for (const { key, value } of body)
-      data[id - 1][key] = value;
+        for (const { key, value } of body)
+            data[id - 1][key] = value;
 
-    res.json(data[id - 1]);
-  },
+        res.json(data[id - 1]);
+    },
 };
 
 exports.userController = {
-  get(req, res) {
+    get(req, res) {
 
-  },
-  post(req, res) {
+    },
+    post(req, res) {
 
-  },
-  put(req, res) {
+    },
+    put(req, res) {
 
-  },
+    },
 };
 
 exports.accountController = {
-  get(req, res) {
+    get(req, res) {
 
-  },
-  post(req, res) {
+    },
+    post(req, res) {
 
-  },
-  delete(req, res) {
+    },
+    delete(req, res) {
 
-  },
+    },
 };
