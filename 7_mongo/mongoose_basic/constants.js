e42095a4e76e52f91b34af236ffdf7826edde64a
dave <david@lusha.co>
2019-12-07 14:42:10 +0200

    test

--- 7_mongo/mongoose_basic/constants.js
+++ 7_mongo/mongoose_basic/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_HOST: 'cluster0-3mb3k.mongodb.net/test?retryWrites=true&w=majority',
+  DB_HOST: 'cluster0-3mb3k.mongodb.net/db2020?retryWrites=true&w=majority',
   DB_USER: 'db_user',
   DB_PASS: 'db_pass',
 };
